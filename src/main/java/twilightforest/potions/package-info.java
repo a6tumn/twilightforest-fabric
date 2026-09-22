1c90b3d369e44553b13b85abef11d4e2f4e2577c
Autumn <shinigami7x@proton.me>
2026-09-22 09:19:34 -0400

    Translated potions.

--- src/main/java/twilightforest/potions/package-info.java
+++
@@ -1,4 +0,0 @@
-@NullMarked
-package twilightforest.potions;
-
-import org.jspecify.annotations.NullMarked;
---
+++ src/main/kotlin/twilightforest/potions/FrostedEffect.kt
@@ -0,0 +1,36 @@
+package twilightforest.potions
+
+import net.minecraft.server.level.ServerLevel
+import net.minecraft.world.effect.MobEffect
+import net.minecraft.world.effect.MobEffectCategory
+import net.minecraft.world.entity.LivingEntity
+import net.minecraft.world.entity.ai.attributes.AttributeModifier
+import net.minecraft.world.entity.ai.attributes.Attributes
+import twilightforest.TFCommon
+import kotlin.math.min
+
+class FrostedEffect : MobEffect(MobEffectCategory.HARMFUL, 0x56CBFD) {
+	companion object {
+		@JvmField
+		val MOVEMENT_SPEED_MODIFIER = TFCommon.prefix("frosted_slowdown")
+		const val FROST_MULTIPLIER = -0.15
+	}
+
+	init {
+		addAttributeModifier(Attributes.MOVEMENT_SPEED, MOVEMENT_SPEED_MODIFIER, FROST_MULTIPLIER, AttributeModifier.Operation.ADD_MULTIPLIED_TOTAL)
+	}
+
+	override fun applyEffectTick(
+		serverLevel: ServerLevel,
+		mob: LivingEntity,
+		amplification: Int
+	): Boolean {
+		mob.setIsInPowderSnow(true)
+		if (amplification > 0 && mob.canFreeze()) {
+			mob.ticksFrozen = min(mob.ticksRequiredToFreeze, mob.ticksFrozen + amplification)
+		}
+		return true
+	}
+
+	override fun shouldApplyEffectTickThisTick(tickCount: Int, amplification: Int): Boolean = true
+}
